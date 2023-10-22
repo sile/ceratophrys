997bd840a144261a6704ea251a85ee9e79873ce3
Takeru Ohta <phjgt308@gmail.com>
2023-10-22 19:49:11 +0900

    Add line module

---
+++ src/line.rs
@@ -0,0 +1,73 @@
+use crate::{Canvas, Color, Point, Render};
+
+#[derive(Debug, Clone, Copy)]
+pub struct Line {
+    point: Point,
+    color: Color,
+}
+
+impl Line {
+    pub const fn new(point: Point, color: Color) -> Self {
+        Self { point, color }
+    }
+
+    pub fn points(self) -> impl Iterator<Item = Point> {
+        let p0 = Point::ORIGIN;
+        let p1 = self.point;
+        let dx = (p1.x - p0.x).abs() + 1;
+        let dy = (p1.y - p0.y).abs() + 1;
+        let sign_y = if p1.y > p0.y { 1 } else { -1 };
+        let sign_x = if p1.x > p0.x { 1 } else { -1 };
+        let (f, r, n, v0, sign0, mut v1, sign1) = if dx > dy {
+            let f = xy as fn(i16, i16) -> Point;
+            let r = Rational::new(dx, dy);
+            (f, r, dx, p0.x, sign_x, p0.y, sign_y)
+        } else {
+            let f = yx as fn(i16, i16) -> Point;
+            let r = Rational::new(dy, dx);
+            (f, r, dy, p0.y, sign_y, p0.x, sign_x)
+        };
+        (0..n).map(move |i| {
+            if i != 0 && (i - 1) / r != i / r {
+                v1 += sign1;
+            }
+            f(v0 + i * sign0, v1)
+        })
+    }
+}
+
+impl Render for Line {
+    fn render(&self, point: Point, canvas: &mut Canvas) {
+        for p in self.points() {
+            canvas.set_pixel(point + p, self.color);
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+struct Rational {
+    num: i16,
+    den: i16,
+}
+
+impl Rational {
+    const fn new(num: i16, den: i16) -> Self {
+        Self { num, den }
+    }
+}
+
+impl std::ops::Div<Rational> for i16 {
+    type Output = i16;
+
+    fn div(self, rhs: Rational) -> Self::Output {
+        self * rhs.den / rhs.num
+    }
+}
+
+fn xy(x: i16, y: i16) -> Point {
+    Point::new(x, y)
+}
+
+fn yx(y: i16, x: i16) -> Point {
+    Point::new(x, y)
+}
--- src/rectangle.rs
+++ src/rectangle.rs
@@ -35,13 +35,20 @@ impl Render for Rectangle {
                 canvas.set_pixel(point + p, self.color);
             }
         } else if !self.size.is_empty() {
+            let mut pixels = Vec::new();
             for x in 0..self.size.width {
-                canvas.set_pixel(point.move_x_unsigned(x), self.color);
-                canvas.set_pixel(point.move_xy_unsigned(x, self.size.height - 1), self.color);
+                pixels.push((point.move_x_unsigned(x), self.color));
+                pixels.push((point.move_xy_unsigned(x, self.size.height - 1), self.color));
             }
             for y in 0..self.size.height {
-                canvas.set_pixel(point.move_y_unsigned(y), self.color);
-                canvas.set_pixel(point.move_xy_unsigned(self.size.width - 1, y), self.color);
+                pixels.push((point.move_y_unsigned(y), self.color));
+                pixels.push((point.move_xy_unsigned(self.size.width - 1, y), self.color));
+            }
+            pixels.sort();
+            pixels.dedup();
+
+            for (point, color) in pixels {
+                canvas.set_pixel(point, color);
             }
         }
     }
