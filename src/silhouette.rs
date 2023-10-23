b99f5fbfbd13b2f65ac74b6b34fb81e28d2b42fb
Takeru Ohta <phjgt308@gmail.com>
2023-10-23 23:44:48 +0900

    Update silhouette module

--- src/silhouette.rs
+++ src/silhouette.rs
@@ -14,7 +14,10 @@ impl<T> Silhouette<T> {
 impl<T: Render> Render for Silhouette<T> {
     fn render(&self, offset: Point, canvas: &mut Canvas) {
         let image = self.image.to_image();
-        for (point, _color) in image.pixels() {
+        for (point, color) in image.pixels() {
+            if color.is_transparent() {
+                continue;
+            }
             canvas.set_pixel(point + offset, Color::rgb_hex(0x000000));
         }
     }
