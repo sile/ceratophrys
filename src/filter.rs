5dbfd37ecf24fea4f1f2b76370ffe79b5d46909e
Takeru Ohta <phjgt308@gmail.com>
2023-10-30 00:36:22 +0900

    Implement filter for animation

--- src/filter.rs
+++ src/filter.rs
@@ -1,3 +1,19 @@
+use crate::Animation;
+
 pub trait Filter<T> {
     fn filter(&self, target: T) -> T;
+}
+
+impl<T, F> Filter<Animation<F>> for T
+where
+    T: Filter<F>,
+{
+    fn filter(&self, mut target: Animation<F>) -> Animation<F> {
+        target.frames = target
+            .frames
+            .into_iter()
+            .map(|frame| self.filter(frame))
+            .collect();
+        target
+    }
 }
