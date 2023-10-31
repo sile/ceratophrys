6f94f4ae4bee4462c6bd7caa7f25e3ba2a4f5e95
Takeru Ohta <phjgt308@gmail.com>
2023-10-31 22:27:18 +0900

    Add xy() and square()

--- src/lib.rs
+++ src/lib.rs
@@ -20,3 +20,11 @@ pub use image::Image;
 pub use pixel::Pixel;
 pub use position::Position;
 pub use size::Size;
+
+pub const fn xy(x: i16, y: i16) -> Position {
+    Position::xy(x, y)
+}
+
+pub const fn square(side: u16) -> Size {
+    Size::square(side)
+}
